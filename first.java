1fc0546afa5732454d0c02b30f060c35ba99a84b
RaviAnand111 <yravianand111@gmail.com>
2020-06-26 13:00:16 +0530

    master commit

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 public class first{
   public static void main(String args[]){
    System.out.println("First Commit");
-   System.out.println("In brach dev and first commit as well");
+   System.out.println("In master branch and first commit as well");
   }
 }
